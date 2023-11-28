784c8a3d3fe262a5c0d8f5e8411bcf4659132985
Mehrshad <code.rezaei@gmail.com>
2023-11-28 12:18:13 +0330

    FSharpLint.Core.Test: add a failure test
    Co-authored-by: webwarrior <reg@webwarrior.ws>

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/AsyncExceptionWithoutReturn.fs
@@ -0,0 +1,78 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.AsyncExceptionWithoutReturn
+
+open NUnit.Framework
+open FSharpLint.Framework.Rules
+open FSharpLint.Rules
+
+[<TestFixture>]
+type TestAsyncExceptionWithoutReturn() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(AsyncExceptionWithoutReturn.rule)
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturn() = 
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction = async {
+    raise (new System.Exception("An error occurred."))
+    return true
+    }""")
+
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturn_2() = 
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction = async {
+    return raise (new System.Exception("An error occurred."))
+    }""")
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturnOnFailWith() =          
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction = async {
+    failwith "An error occurred."
+    return true
+    }""")
+
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturnOnFailWith_2() =          
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction = async {
+    failwithf "An error occurred."
+    return true
+    }""")
+
+        Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturnOnFailWithf() = 
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction = async {
+    return failwith "An error occurred."
+    }""")
+
+        this.AssertNoWarnings()
+
+    [<Test>]
+    member this.AsyncExceptionWithoutReturnOnFailWithf_2() = 
+        this.Parse("""
+namespace Program
+
+let someAsyncFunction = async {
+    return failwithf "An error occurred."
+    }""")
+
+        this.AssertNoWarnings()
