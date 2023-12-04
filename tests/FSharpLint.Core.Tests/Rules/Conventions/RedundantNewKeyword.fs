d2580ce3180d1d6ea0fce7cbf9b8063c7139ab31
Mehrshad <code.rezaei@gmail.com>
2023-12-04 12:15:25 +0330

    testing issue 572

--- tests/FSharpLint.Core.Tests/Rules/Conventions/RedundantNewKeyword.fs
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/RedundantNewKeyword.fs
@@ -66,3 +66,22 @@ module Program
         new Int32() |> ignore""")
 
         Assert.IsTrue this.ErrorsExist
+
+    [<Test>]
+    member this.``issue 522 test``() =
+        this.Parse("""
+open System
+
+type ISomeInterfaceWithDisposable =
+    interface
+        inherit IDisposable
+    end
+
+type SomeDisposableType() =
+    interface ISomeInterfaceWithDisposable with
+        member _.Dispose() = ()
+
+module Program =
+    let foo = new SomeDisposableType() :> ISomeInterfaceWithDisposable""")
+
+        this.AssertNoWarnings()
