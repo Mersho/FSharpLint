598723cb3e7f1b30c00d1e0c6005ca79e0659a37
webwarrior <reg@webwarrior.ws>
2023-12-04 14:52:11 +0100

    Core(Tests): new rule and tests for it
    Add a rule that suggest usage of auto-property for property
    with getter that only returns some (immutable) value. Add
    tests for it.

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/SuggestUseAutoProperty.fs
@@ -0,0 +1,27 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.SuggestUseAutoProperty
+
+open NUnit.Framework
+open FSharpLint.Framework.Rules
+open FSharpLint.Rules
+
+[<TestFixture>]
+type TestSuggestUseAutoProperty() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(SuggestUseAutoProperty.rule)
+
+    [<Test>]
+    member this.``Should suggest usage of auto-property for property that only returns immutable value`` () =
+        this.Parse """
+type Foo(content: int) =
+    member x.Content = content
+"""
+
+        Assert.IsTrue(this.ErrorsExist)
+
+    [<Test>]
+    member this.``Should suggest usage of auto-property for property that only returns literal`` () =
+        this.Parse """
+type Foo() =
+    member x.Content = 42
+"""
+
+        Assert.IsTrue(this.ErrorsExist)
