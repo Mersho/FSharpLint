84a75e56982849dbbe9ed311e6a2acc932fca53c
Mehrshad <code.rezaei@gmail.com>
2023-12-28 14:51:01 +0330

    Core: implement new rule

--- src/FSharpLint.Core/Rules/Conventions/UnneededMutableKeyword.fs
+++ src/FSharpLint.Core/Rules/Conventions/UnneededMutableKeyword.fs
@@ -7,9 +7,30 @@ open FSharpLint.Framework.Rules
 open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 
-let runner (args:AstNodeRuleParams) =
-    printfn "%A" args.AstNode
-    failwith "not implemented"
+let runner (args: AstNodeRuleParams) =
+    match args.AstNode, args.CheckInfo with
+    | AstNode.ModuleDeclaration (SynModuleDecl.Let (_, bindings, _)), Some checkInfo ->
+        match bindings with
+        | SynBinding (_, _, _, isMutable, _, _, _, SynPat.Named (_, ident, _, _, varRange), _, _, _, _) :: _ when
+            isMutable
+            ->
+            let symbolUses = checkInfo.GetAllUsesOfAllSymbolsInFile()
+            let varName = ident.idText
+
+            let varNameUsages =
+                symbolUses
+                |> Seq.filter (fun usage -> usage.Symbol.DisplayName = varName)
+
+            if (varNameUsages |> Seq.length) <= 1 then
+                { Range = varRange
+                  Message = String.Format(Resources.GetString "RulesUnneededMutableKeyword", varName)
+                  SuggestedFix = None
+                  TypeChecks = list.Empty }
+                |> Array.singleton
+            else
+                Array.empty
+        | _ -> Array.empty
+    | _ -> Array.empty
 
 let rule =
     { Name = "UnneededMutableKeyword"
