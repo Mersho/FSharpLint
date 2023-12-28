585ace566d476366189707272b557b56aaab4fd1
Mehrshad <code.rezaei@gmail.com>
2023-12-28 12:40:50 +0330

    Core: introduce new rule

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/UnneededMutableKeyword.fs
@@ -0,0 +1,24 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.UnneededMutableKeyword
+
+open NUnit.Framework
+open FSharpLint.Framework.Rules
+open FSharpLint.Rules
+
+[<TestFixture>]
+type TestConventionsUnneededMutableKeyword() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(UnneededMutableKeyword.rule)
+
+    [<Test>]
+    member this.UnneededMutableKeywordShouldNotProduceError() =
+        this.Parse """
+let mutable foo = 1
+foo <- foo + 1"""
+
+        Assert.IsTrue this.NoErrorsExist
+
+    [<Test>]
+    member this.UnneededMutableKeywordShouldProduceError() =
+        this.Parse """
+let mutable foo = 1"""
+
+        Assert.IsTrue this.ErrorsExist
