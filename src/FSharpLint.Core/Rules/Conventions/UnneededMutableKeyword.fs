2f3d12299ea001b5aa5b7b8b8f477a015f3b1cf3
Mehrshad <code.rezaei@gmail.com>
2023-12-28 12:40:50 +0330

    Core: introduce new rule

---
+++ src/FSharpLint.Core/Rules/Conventions/UnneededMutableKeyword.fs
@@ -0,0 +1,20 @@
+﻿module FSharpLint.Rules.UnneededMutableKeyword
+
+open System
+open FSharp.Compiler.Syntax
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+
+let runner (args:AstNodeRuleParams) =
+    printfn "%A" args.AstNode
+    failwith "not implemented"
+
+let rule =
+    { Name = "UnneededMutableKeyword"
+      Identifier = Identifiers.UnneededMutableKeyword
+      RuleConfig =
+        { AstNodeRuleConfig.Runner = runner
+          Cleanup = ignore } }
+    |> AstNodeRule
