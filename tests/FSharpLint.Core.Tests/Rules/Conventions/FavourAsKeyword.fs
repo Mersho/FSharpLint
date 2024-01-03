7867003941f8e498baffa647c04f8bff346b35bf
Mehrshad <code.rezaei@gmail.com>
2024-01-03 13:35:59 +0330

    Core: introduce new rule

---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/FavourAsKeyword.fs
@@ -0,0 +1,29 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.FavourAsKeyword
+
+open NUnit.Framework
+open FSharpLint.Framework.Rules
+open FSharpLint.Rules
+
+[<TestFixture>]
+type TestConventionsFavourAsKeyword() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(FavourAsKeyword.rule)
+
+    [<Test>]
+    member this.FavourAsKeywordShouldNotProduceError() =
+        this.Parse """
+let foo = "baz"
+match foo with
+| "baz" as bar -> printfn "bar is %s" bar
+| _ -> printfn "bar is not baz" """
+
+        Assert.IsTrue this.NoErrorsExist
+
+    [<Test>]
+    member this.FavourAsKeywordShouldProduceError_1() =
+        this.Parse """
+let foo = "baz"
+match foo with
+| bar when bar = "baz" -> printfn "bar is baz"
+| _ -> printfn "bar is not baz" """
+
+        Assert.IsTrue this.ErrorsExist
