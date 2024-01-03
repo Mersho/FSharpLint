961bc83f4c10791a6ebd4ea148e056f718362253
Mehrshad <code.rezaei@gmail.com>
2024-01-03 14:35:51 +0330

    Core: implement new rule

--- src/FSharpLint.Core/Rules/Conventions/FavourAsKeyword.fs
+++ src/FSharpLint.Core/Rules/Conventions/FavourAsKeyword.fs
@@ -8,8 +8,29 @@ open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 
 let runner (args: AstNodeRuleParams) =
-    printfn "%A" args.AstNode
-    failwith "not implemented"
+    match args.AstNode with
+    | AstNode.Expression(SynExpr.Match(_, _, clauses, _)) ->
+        let wrongMatchExpressions =
+            clauses
+            |> List.filter (fun clause ->
+                match clause with
+                | SynMatchClause(SynPat.Named(_, ident, _, _, _), Some(whenExpr), _, _, _) ->
+                    match whenExpr with
+                    | SynExpr.App(_, _, SynExpr.App(_, _, _, SynExpr.Ident conditionIdent, _), _, _) ->
+                        conditionIdent.idText = ident.idText
+                    | _ -> false
+                | _ -> false)
+
+        if wrongMatchExpressions.Length > 0 then
+            { Range = wrongMatchExpressions.Head.Range
+              Message = Resources.GetString "RulesFavourAsKeyword"
+              SuggestedFix = None
+              TypeChecks = list.Empty }
+            |> Array.singleton
+        else
+            Array.empty
+
+    | _ -> Array.empty
 
 let rule =
     { Name = "FavourAsKeyword"
