ce09fa7c9db22cda272c620f1c4cb1e91f18f99b
Mehrshad <code.rezaei@gmail.com>
2024-01-03 13:35:59 +0330

    Core: introduce new rule

---
+++ src/FSharpLint.Core/Rules/Conventions/FavourAsKeyword.fs
@@ -0,0 +1,20 @@
+﻿module FSharpLint.Rules.FavourAsKeyword
+
+open System
+open FSharp.Compiler.Syntax
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+
+let runner (args: AstNodeRuleParams) =
+    printfn "%A" args.AstNode
+    failwith "not implemented"
+
+let rule =
+    { Name = "FavourAsKeyword"
+      Identifier = Identifiers.FavourAsKeyword
+      RuleConfig =
+        { AstNodeRuleConfig.Runner = runner
+          Cleanup = ignore } }
+    |> AstNodeRule
