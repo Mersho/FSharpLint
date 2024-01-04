f5b2978927995252b288885b4fb7aa2490fc4117
Mehrshad <code.rezaei@gmail.com>
2024-01-04 17:10:49 +0330

    FavourAsKeyword: implement suggestions

--- src/FSharpLint.Core/Rules/Conventions/FavourAsKeyword.fs
+++ src/FSharpLint.Core/Rules/Conventions/FavourAsKeyword.fs
@@ -10,24 +10,43 @@ open FSharpLint.Framework.Suggestion
 let runner (args: AstNodeRuleParams) =
     match args.AstNode with
     | AstNode.Expression(SynExpr.Match(_, _, clauses, _)) ->
-        let wrongMatchExpressions =
-            clauses
-            |> List.filter (fun clause ->
+        Array.concat
+            (clauses
+            |> List.map (fun clause ->
                 match clause with
-                | SynMatchClause(SynPat.Named(_, ident, _, _, _), Some(whenExpr), _, _, _) ->
+                | SynMatchClause(SynPat.Named(_, ident, _, _, _), Some(whenExpr), resultExpr, range, _) ->
                     match whenExpr with
-                    | SynExpr.App(_, _, SynExpr.App(_, _, _, SynExpr.Ident conditionIdent, _), _, _) ->
+                    | SynExpr.App(_, _, SynExpr.App(_, _, _, SynExpr.Ident conditionIdent, _), argExpr, _) when
                         conditionIdent.idText = ident.idText
-                    | _ -> false
-                | _ -> false)
+                        ->
+                        match argExpr with
+                        | SynExpr.Const(_, constRange) ->
+                            let constText = ExpressionUtilities.tryFindTextOfRange constRange args.FileContent
+                            let matchText = ExpressionUtilities.tryFindTextOfRange range args.FileContent
+                            let resultExprText = ExpressionUtilities.tryFindTextOfRange resultExpr.Range args.FileContent
 
-        wrongMatchExpressions
-        |> List.map (fun expr ->
-            { Range = expr.Range
-              Message = Resources.GetString "RulesFavourAsKeyword"
-              SuggestedFix = None
-              TypeChecks = list.Empty })
-        |> List.toArray
+                            let suggestedFix =
+                                lazy
+                                    (match constText, matchText, resultExprText with
+                                     | Some cText, Some mText, Some rExprText ->
+                                         let toText = sprintf "%s as %s -> %s" (cText) (ident.idText) (rExprText)
+
+                                         Some(
+                                             { FromText = mText
+                                               FromRange = range
+                                               ToText = toText }
+                                         )
+                                     | _ -> None)
+
+                            { Range = range
+                              Message = Resources.GetString "RulesFavourAsKeyword"
+                              SuggestedFix = Some(suggestedFix)
+                              TypeChecks = list.Empty }
+                            |> Array.singleton
+                        | _ -> Array.empty
+                    | _ -> Array.empty
+                | _ -> Array.empty)
+            )
 
     | _ -> Array.empty
 
