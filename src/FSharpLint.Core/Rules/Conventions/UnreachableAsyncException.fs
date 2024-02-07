71c5f50852cca1e2aff90c670973c0c2dfb19695
Mehrshad <code.rezaei@gmail.com>
2024-02-07 14:54:24 +0330

    Core: introduce new rule

---
+++ src/FSharpLint.Core/Rules/Conventions/UnreachableAsyncException.fs
@@ -0,0 +1,24 @@
+﻿module FSharpLint.Rules.UnreachableAsyncException
+
+open System
+open FSharp.Compiler.Syntax
+open FSharpLint.Framework.Ast
+open FSharpLint.Framework.Rules
+open FSharpLint.Framework
+open FSharpLint.Framework.Suggestion
+
+let runner (args: AstNodeRuleParams) =
+    printfn "%A" args.AstNode
+    failwith "Not implemented yet."
+
+let rule =
+    {
+        Name = "UnreachableAsyncException"
+        Identifier = Identifiers.UnreachableAsyncException
+        RuleConfig =
+            {
+                AstNodeRuleConfig.Runner = runner
+                Cleanup = ignore
+            }
+    }
+    |> AstNodeRule
---
+++ tests/FSharpLint.Core.Tests/Rules/Conventions/UnreachableAsyncException.fs
@@ -0,0 +1,35 @@
+﻿module FSharpLint.Core.Tests.Rules.Conventions.UnreachableAsyncException
+
+open NUnit.Framework
+open FSharpLint.Rules
+
+[<TestFixture>]
+type TestConventionsUnreachableAsyncExceptionKeyword() =
+    inherit TestAstNodeRuleBase.TestAstNodeRuleBase(UnreachableAsyncException.rule)
+
+    [<Test>]
+    member this.UnreachableAsyncExceptionShouldNotProduceError() =
+        this.Parse """
+let Foo(): Async<Result<'Bar, string>> =
+    async {
+        try
+            let! result = DoSomething()
+            return Ok(result)
+        with
+        | ex -> return Error("An error occurred: " + ex.Message)
+    }
+"""
+
+        Assert.IsTrue this.NoErrorsExist
+
+    [<Test>]
+    member this.UnreachableAsyncExceptionShouldProduceError() =
+        this.Parse """
+let Foo() =
+    try
+        1
+    with
+    | ex -> failwith "exception raised on DoSomething()"
+"""
+
+        Assert.IsTrue this.ErrorsExist
