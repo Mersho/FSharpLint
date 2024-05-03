83e318278d33b3f4f77ab039ae9f0043c3dacca0
Mehrshad <code.rezaei@gmail.com>
2024-05-03 09:04:46 +0330

    Core: implement new rule

--- src/FSharpLint.Core/Rules/Conventions/AvoidDoubleLookupDictionaries.fs
+++ src/FSharpLint.Core/Rules/Conventions/AvoidDoubleLookupDictionaries.fs
@@ -7,8 +7,31 @@ open FSharpLint.Framework
 open FSharpLint.Framework.Suggestion
 
 let runner (args: AstNodeRuleParams) =
-    printfn "%A" args.AstNode
-    failwith "not implemented"
+    match args.AstNode with
+    | AstNode.Expression(SynExpr.Match(_, expr, _, range, _)) ->
+        match expr with
+        | SynExpr.App(_, _, SynExpr.LongIdent(_, synLongIdent, _, _), _, _) ->
+            if (synLongIdent.LongIdent |> List.last).idText = "ContainsKey" then
+                let dictVar = synLongIdent.LongIdent |> List.head
+
+                let isDictFound =
+                    ExpressionUtilities.tryFindTextOfRange range args.FileContent
+                    |> Option.map (fun text -> text.Contains(sprintf "%s.[" dictVar.idText))
+
+                match isDictFound with
+                | Some _ ->
+                    {
+                        Range = range
+                        Message = Resources.GetString "RulesAvoidDoubleLookupDictionariesError"
+                        SuggestedFix = None
+                        TypeChecks = List.Empty
+                    }
+                    |> Array.singleton
+                | None -> Array.empty
+            else
+                Array.empty
+        | _ -> Array.empty
+    | _ -> Array.empty
 
 let rule =
     {
